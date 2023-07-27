d4c3c39883645be1755db375363b20641473183f
Vyacheslav Sosnovskii <doctorveb292@icloud.com>
2023-07-27 23:12:19 +0300

    add fixed js

--- index.js
+++ index.js
@@ -32,3 +32,20 @@ burgerBtn.forEach(function(el) {
     document.body.classList.remove('stop-scroll');
   })
 })
+
+// Плавный скролл
+
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href').substr(1)
+
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+}
