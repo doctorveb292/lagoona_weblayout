6ace5672580f66935a2e3070a893c51335462cf0
Vyacheslav Sosnovskii <doctorveb292@icloud.com>
2023-07-27 23:04:29 +0300

    add fixed media and js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// Burger 
+// Burger
 
 let burger = document.querySelector('.burger');
 let menu = document.querySelector('.header-nav');
 let menulinks = menu.querySelectorAll('.header-link');
+let burgerBtn = document.querySelectorAll('.header-btn-1');
 
 burger.addEventListener('click',
 
@@ -15,6 +16,15 @@ function() {
 })
 
 menulinks.forEach(function(el) {
+  el.addEventListener('click', function(){
+    burger.classList.remove('burger--active');
+    menu.classList.remove('header-nav--active');
+
+    document.body.classList.remove('stop-scroll');
+  })
+})
+
+burgerBtn.forEach(function(el) {
   el.addEventListener('click', function(){
     burger.classList.remove('burger--active');
     menu.classList.remove('header-nav--active');
